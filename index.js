201cd95b993df0f44a472e41500c172ff3e289e2
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-05-08 16:19:11 -0300

    Adicionando método para buscar

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ salvar(pessoa);
 
 async function salvar(pessoa){
     // await client.connect();
-    const retorno = await client.set(pessoa.email, JSON.stringify(pessoa));
+    const retorno = await client.set(pessoa.email, JSON.stringify(pessoa),{
+        EX: 30
+    });
     console.log(retorno);
+}
+
+// buscar('joao@gmail.com');
+
+async function buscar(email){
+    const retorno = await client.get(email);
+    console.log(retorno);
+    const pessoa = JSON.parse(retorno);
+    console.log(pessoa);
 }

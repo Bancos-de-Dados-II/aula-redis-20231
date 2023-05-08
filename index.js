5e34e97e4901065e8c4572aba5d9df35fb38a607
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-05-08 15:51:28 -0300

    Adicionando método para setar

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const redis = require('redis');
-
 const client = redis.createClient();
 
 conectar();
@@ -7,5 +6,18 @@ conectar();
 async function conectar(){
     client.on('error', err => console.log('Redis Client Error', err));
     await client.connect();
-    console.log('Conectado');
+    console.log(await client.ping());
+}
+
+const pessoa = {
+    nome: 'João',
+    email: 'joao@gmail.com'
+}
+
+salvar(pessoa);
+
+async function salvar(pessoa){
+    // await client.connect();
+    const retorno = await client.set(pessoa.email, JSON.stringify(pessoa));
+    console.log(retorno);
 }
